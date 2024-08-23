1f76e0102fe0b646afba9a008228f1940cd8220b
Oosh74 <usherjared@yahoo.com>
2024-08-22 23:24:40 -0400

    Add solutions for unit 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let strArr = string.split('');
+  let reverseStrArr = [];
 
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    reverseStrArr.push(strArr[i]);
+  }
+
+  return reverseStrArr.join('');
 };
 
 // Do not edit below this line
