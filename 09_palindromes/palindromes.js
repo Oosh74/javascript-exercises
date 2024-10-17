32312290599eabb3ade9e16fd1c75870df09af26
Oosh74 <usherjared@yahoo.com>
2024-10-16 20:26:29 -0400

    Palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = (string) => {
+  let sanitizedStr = string
+    .replace(/[.,/#!$%^&*;:{}=\-_`~()]/g, '')
+    .replace(/\s/g, '')
+    .toLowerCase();
 
+  let newString = sanitizedStr.split('').reverse().join('');
+
+  console.log(newString);
+  if (sanitizedStr === newString) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(palindromes('racecar!'));
 
 // Do not edit below this line
 module.exports = palindromes;
