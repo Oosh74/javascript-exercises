09bbad8a9b8824420432e0f23b70cfe666f219e5
Oosh74 <usherjared@yahoo.com>
2024-11-03 23:20:11 -0500

    Fibonacci solution complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num === 'string') {
+    num = Number(num);
+  }
 
+  if (num < 0) {
+    return 'OOPS';
+  }
+
+  if (num === 1) {
+    return 1;
+  }
+
+  let currNum = 1;
+  let prevNum = 0;
+  let newSum = 0;
+
+  for (let i = 1; i < num; i++) {
+    newSum = prevNum + currNum;
+    prevNum = currNum;
+    currNum = newSum;
+  }
+
+  return newSum;
 };
+
+console.log(fibonacci(1));
 
 // Do not edit below this line
 module.exports = fibonacci;
