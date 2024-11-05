010dac7c6547422236960574492db5144697a11e
Oosh74 <usherjared@yahoo.com>
2024-11-04 22:18:09 -0500

    solution for excercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+  let titleArr = [];
 
+  array.map((arr) => {
+    titleArr.push(arr.title);
+  });
+
+  return titleArr;
 };
 
 // Do not edit below this line
